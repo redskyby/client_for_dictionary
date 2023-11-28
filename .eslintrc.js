612c0c45a34e5be7fb381be167ba7d32f708499d
Docenko Pasha <pashadocenko@gmail.com>
2023-11-28 23:46:37 +0300

    Eslint and prettier were added.

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  rules: {
+    "prettier/prettier": ["warn", { semi: true }],
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
