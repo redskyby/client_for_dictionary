aabbe86e5766c2f440bbd19f07dadb0c9709df98
Docenko Pasha <pashadocenko@gmail.com>
2023-12-01 22:21:18 +0300

    Auth.tsx was started to init.

--- src/page/auth/Auth.tsx
+++ src/page/auth/Auth.tsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Auth.module.scss";
 
 const Auth = () => {
-    return <div>я форма регистрации</div>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("я работаю!");
+    };
+    return (
+        <div className={style.container}>
+            <div>
+                <h2>Авторизация</h2>
+            </div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                    Email :
+                    <input
+                        type={"email"}
+                        value={email}
+                        onChange={(e) => handleEmailChange(e)}
+                        placeholder={"Введите свой email"}
+                        required
+                    />
+                </label>
+                <label>
+                    Password :
+                    <input
+                        type={"password"}
+                        value={password}
+                        onChange={(e) => handlePasswordChange(e)}
+                        placeholder={"Введите свой password"}
+                        required
+                    />
+                </label>
+                <button type={"submit"}>Войти</button>
+            </form>
+        </div>
+    );
 };
 
 export default Auth;
