48459e5a484d390cc7fd2396d6c2a9ed05c2da5d
Docenko Pasha <pashadocenko@gmail.com>
2023-12-09 23:46:17 +0300

    Dynamic pagination was added for ListOfAllWords.tsx.

--- src/page/listOfAllWords/ListOfAllWords.tsx
+++ src/page/listOfAllWords/ListOfAllWords.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./ListOfAllWords.module.scss";
 import { useGetAllWordsQuery } from "../../redux/query/AllWordsQuery";
 import { RingLoader } from "react-spinners";
@@ -6,6 +6,27 @@ import { RingLoader } from "react-spinners";
 const ListOfAllWords = () => {
     const [count, setCount] = useState<number>(1);
     const { data, isLoading, error } = useGetAllWordsQuery(count);
+    const observer = useRef<IntersectionObserver | null>(null);
+    const lastElement = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (data) {
+            if (observer.current) {
+                observer.current.disconnect();
+            }
+            const callBack: IntersectionObserverCallback = (entries) => {
+                if (entries[0].isIntersecting && count <= data.totalCount) {
+                    setCount((prevCount) => prevCount + 1);
+                }
+            };
+
+            observer.current = new IntersectionObserver(callBack);
+
+            if (lastElement.current) {
+                observer.current.observe(lastElement.current);
+            }
+        }
+    }, [count, data]);
 
     return (
         <div className={style.container}>
@@ -19,9 +40,14 @@ const ListOfAllWords = () => {
                 </div>
             ) : data ? (
                 <div className={style.list_all_words}>
-                    <button onClick={() => setCount(count + 1)}>+</button>
+                    <div className={style.list_all_words_main_title}>
+                        <h2>Список всех слов</h2>
+                        <h3>Всего слов : {data?.totalCount}</h3>
+                    </div>
+                    <div></div>
                 </div>
             ) : null}
+            <div ref={lastElement}></div>
         </div>
     );
 };
--- src/redux/query/AllWordsQuery.ts
+++ src/redux/query/AllWordsQuery.ts
@@ -1,11 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Word } from "../../services/Interfeces";
 
 export const AllWordsQuery = createApi({
     reducerPath: "AllWordsQuery",
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
     endpoints: (build) => ({
-        getAllWords: build.query<Word[], number>({
+        getAllWords: build.query({
             query: (count) => `api/word/getAll/?count=${count}`,
         }),
     }),
