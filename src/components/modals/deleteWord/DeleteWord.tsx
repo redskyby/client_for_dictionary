028e3774ad16263be866d29e5d080855d0ef17a9
Docenko Pasha <pashadocenko@gmail.com>
2024-01-02 22:42:56 +0300

    DeleteWord.tsx was added.

---
+++ src/components/modals/deleteWord/DeleteWord.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./DeleteWord.module.scss";
+
+const DeleteWord = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DeleteWord;
