94afff0057d21b8da1ec83290ac899f9666d33e8
Docenko Pasha <pashadocenko@gmail.com>
2024-01-02 22:40:03 +0300

    AddWord.tsx was added.

---
+++ src/components/modals/addWord/AddWord.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "AddWord.module.scss";
+
+const AddWord = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AddWord;
