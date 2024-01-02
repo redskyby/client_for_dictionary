b7192bb7ea05de47897e29b6a8d81fb9c564cafc
Docenko Pasha <pashadocenko@gmail.com>
2024-01-02 22:41:38 +0300

    PutWord.tsx was added.

---
+++ src/components/modals/putWord/PutWord.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./PutWord.module.scss";
+
+const PutWord = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PutWord;
